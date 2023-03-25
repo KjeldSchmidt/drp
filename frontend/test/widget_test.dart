19bd128dfb160891b2b14bedf6f27b0067a06c97
Kjeld Schmidt <kjeld.schmidt@thoughtworks.com>
2023-03-26 00:03:04 +0100

    Fixed test

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -11,20 +11,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:drop/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('The navbar has the tagline', (WidgetTester tester) async {
+    await tester.pumpWidget(const App());
+    expect(find.text('Drop Messages Anywhere'), findsOneWidget);
   });
 }
