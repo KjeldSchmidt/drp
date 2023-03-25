237c348db7a21f487bc5686702117731cd4eab43
Kjeld Schmidt <kjeld.schmidt@thoughtworks.com>
2023-03-25 23:50:29 +0100

    Cleaned up some early autogenerated stuff

--- frontend/lib/NetworkService.dart
+++
@@ -1,7 +0,0 @@
-import 'package:http/http.dart' as http;
-
-class NetworkService {
-  static Future<http.Response> fetchText() {
-    return http.get(Uri.parse("https://fakerapi.it/api/v1/addresses?_quantity=1"));
-  }
-}
